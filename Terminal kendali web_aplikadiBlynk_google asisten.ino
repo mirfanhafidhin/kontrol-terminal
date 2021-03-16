99b26e7ebd6e6efe0ab80384890c350c657dd5db
mirfanhafidhin <80776101+mirfanhafidhin@users.noreply.github.com>
2021-03-16 23:20:42 +0700

    Terminal cerdas
    Terminal yang bisa di kendalikan dengan 3 fitur kendali yaitu melalui aplikasi mobile,web dan Google asisten.

---
+++ Terminal kendali web_aplikadiBlynk_google asisten.ino
@@ -0,0 +1,43 @@
+#define BLYNK_PRINT Serial
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <BlynkSimpleEsp8266.h>
+
+ESP8266 Server WebServer (80);
+String webpage = "<H1> Belajar <H1> <p> <a href=\"on\"> <button> di </button> </a> & nbsp & nbsp; <a href=\"off\"> <button > mati </button> </a> </p> & nbsp & nbsp; ";
+char auth [] = "aW4SNEmR_XyQ2PPN2YO3xGAeRdPaujG1";
+char ssid [] = "MAHASISWATERZOLIMI";
+char pass [] = "iuytrewq";
+batal penyiapan () {
+  // letakkan kode penyiapan Anda di sini, untuk dijalankan sekali:
+ Serial.begin (9600);
+ Serial.println ();
+ pinMode (D5, OUTPUT);
+ WiFi.begin ("MAHASISWATERZOLIM", "iuytrewq");
+ Blynk.begin (auth, ssid, pass);
+
+ Serial.print ("Terhubung");
+ sementara (WiFi.status ()! = WL_CONNECTED)
+ {
+  penundaan (500);
+  Serial.print (".");
+ }
+  Serial.println ();
+  Serial.print ("Terhubung, alamat IP:");
+  Serial.println (WiFi.localIP ());
+
+    server.on ("/ off", [] () {
+    server.send (200, "text / html", halaman web);
+    digitalWrite (D5, TINGGI);
+});
+    server.on ("/ on", [] () {
+    server.send (200, "text / html", halaman web);
+    digitalWrite (D5, LOW);
+});
+server.begin ();
+}
+
+void loop () {
+  Blynk.run ();
+  server.handleClient ();
+}
